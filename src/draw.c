84d0413a788a8b6baba8401ec54bfd4c5a0a4887
Paul Tan <pjxtan@gmail.com>
2014-10-17 20:19:51 -0400

    Added write flag, outfile name string and file pointer; added getopt check for write flag; stdout suppressed if writing to file

--- src/draw.c
+++ src/draw.c
@@ -6,18 +6,22 @@
 #include "fractals.h"
 #include "ifs.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     char fractal[20];
     int c;
     int i, j;
     int n_iter;
     int n_dims;
+    double *points;
     func_ptr func = NULL;
+    int write = 0;
+    char outfile[20];
+    FILE *poutfile;
 
     /* If no arguments given, list fractals */
     if (argc < 2) {
         printf("\nImplemented fractals are:\n");
-        printf(fractal_list);
+        printf("%s", fractal_list);
         print_usage();
         printf("\n");
         return EXIT_SUCCESS;
@@ -25,7 +29,7 @@ int main(int argc, char * argv[]) {
 
     /* Parse arguments */
     opterr = 0;
-    while ((c = getopt(argc, argv, "f:n:")) != -1)
+    while ((c = getopt(argc, argv, "f:n:w")) != -1)
         switch (c) {
             case 'f':
                 strcpy(fractal, optarg);
@@ -33,13 +37,14 @@ int main(int argc, char * argv[]) {
             case 'n':
                 n_iter = atoi(optarg);
                 break;
+            case 'w':
+                write = 1;
+                break;
             /* Need to parse options for generic IFS here... */
             default:
                 print_usage();
                 return EXIT_FAILURE;
         }
-    
-    double * points = malloc(n_dims * n_iter * sizeof(double));
 
     /* If chosen fractal is "generic", apply a generic IFS */
     if (strcmp(fractal, "generic") == 0) {
@@ -53,23 +58,41 @@ int main(int argc, char * argv[]) {
         //double y0 [] = {0.0, 0.5, 0.0};
         double y0 [] = {0.0, 0.0, 1.0, 1.0};
         double init [] = {0.0, 0.0};
-
+        
+        n_dims = 2;
+        points = malloc(n_dims * n_iter * sizeof(double));
         points = ifs_2d(n_iter, n_transforms, beta, x0, y0, init);
     }
     /* If chosen fractal is not "generic", look it up and draw it */
     else {
         func = lookup_func(fractal);
         n_dims = lookup_n_dims(fractal);
+        points = malloc(n_dims * n_iter * sizeof(double));
         points = func(n_iter);
     }
-
+    
+    if (write) {
+        strcpy(outfile, fractal);
+        strcat(outfile, ".dat");
+        poutfile = fopen(outfile, "w");
+    }
     
     for (i = 0; i < (n_dims * n_iter); i += n_dims)
     {
-        for (j = 0; j < n_dims; j++)
-            printf("%f ", points[i + j]);
-        printf("\n");
+        for (j = 0; j < n_dims; j++) {
+            if (write)
+                fprintf(poutfile, "%f ", points[i + j]);
+            else
+                printf("%f ", points[i + j]);
+        }
+        if (write)
+            fprintf(poutfile, "\n");
+        else
+            printf("\n");
     }
+
+    if (write)
+        fclose(poutfile);
 
     return EXIT_SUCCESS;
 }
@@ -78,8 +101,8 @@ void print_usage() {
     printf("Usage: ./draw -f fractal -n n_iter\n");
 }
 
-func_ptr lookup_func(char * fractal) {
-    function_table * ptable;
+func_ptr lookup_func(char *fractal) {
+    function_table *ptable;
 
     for (ptable = func_table; ptable->fractal != NULL; ptable++) {
         if (strcmp(ptable->fractal, fractal) == 0)
@@ -88,12 +111,11 @@ func_ptr lookup_func(char * fractal) {
 
     fprintf(stderr, "Invalid fractal specified\n");
     print_usage();
-
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
 }
 
-int lookup_n_dims(char * fractal) {
-    function_table * ptable;
+int lookup_n_dims(char *fractal) {
+    function_table *ptable;
 
     for (ptable = func_table; ptable->fractal != NULL; ptable++) {
         if (strcmp(ptable->fractal, fractal) == 0)
@@ -102,6 +124,5 @@ int lookup_n_dims(char * fractal) {
 
     fprintf(stderr, "Invalid fractal specified\n");
     print_usage();
-
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
 }
