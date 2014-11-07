ce9b4a4f6f9847d61b27fc9f03649b17e60ec73e
Paul J.X. Tan <pjxtan@gmail.com>
2014-11-07 11:19:46 -0500

    Declared ifs_2d()

--- include/ifs.h
+++ include/ifs.h
@@ -2,6 +2,6 @@
 #define IFS_H
 
 /* A generic 2-dimensional iterated function system. */
-double * ifs_2d(int, int, double [], double [], double [], double []);
+double * ifs_2d(int, int, double [], int [], double []);
 
 #endif /* IFS_H */
