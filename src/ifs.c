dd04268955ffcc586f42bf5a7e4b7b0343596fad
Paul J.X. Tan <pjxtan@gmail.com>
2014-11-07 11:19:33 -0500

    Defined generic 2D IFS function

--- src/ifs.c
+++ src/ifs.c
@@ -1,19 +1,19 @@
 /* Generic iterated function systems. */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "ifs.h"
 
-// TODO:
-//   generalize to accept arbitrary transformations as function arguments
-//   incorporate weights for each transform
-//   incorporate rotations
+/* TODO:
+ *     incorporate rotations
+ */
 
-double * ifs_2d(int n_iter, int n_transforms, double beta [], double x0 [],
-                double y0 [], double init []) {
-    int i;
-    int j;
+double * ifs_2d(int n_iter, int n_transforms, double init[], int probs[],
+                double T[]) {
+    int i, j;
     int test;
+    int prob_used; // Used to set upper limit for probability check
     /* All (x,y) coordinates */
     double * points = malloc(2 * n_iter * sizeof(double));
 
@@ -25,12 +25,24 @@ double * ifs_2d(int n_iter, int n_transforms, double beta [], double x0 [],
 
     /* Iterate */
     for (i = 2; i < (2 * n_iter); i += 2) {
-        test = rand() % n_transforms;
-        /* Choose a transform and apply it */
-        for (j = 0; j < n_transforms; j++) {
-            if (test == j) {
-                points[i]     = beta[j] * (points[i - 2] - x0[j]) + x0[j];
-                points[i + 1] = beta[j] * (points[i - 1] - y0[j]) + y0[j];
+        test = rand() % 100;
+        prob_used = 0;
+
+        /* Check if using first transform */
+        prob_used += probs[0];
+        if (test < prob_used) {
+            points[i]   = T[0] * points[i-2] + T[1] * points[i-1] + T[4];
+            points[i+1] = T[2] * points[i-2] + T[3] * points[i-1] + T[5];
+        }
+
+        /* Check the rest */
+        for (j = 1; j < n_transforms; j++) {
+            prob_used += probs[j];
+            if (test < prob_used) {
+                points[i]   =   T[0+j*6] * points[i-2]
+                              + T[1+j*6] * points[i-1] + T[4+j*6];
+                points[i+1] =   T[2+j*6] * points[i-2]
+                              + T[3+j*6] * points[i-1] + T[5+j*6];
                 break;
             }
         }
